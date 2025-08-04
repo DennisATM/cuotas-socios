d2fa8b8a30f22bcc4eea484d0707ffbae7da5926
Dennis Torres <dennisalbertotm@gmail.com>
2025-08-04 12:10:44 -0400

    Agregando DB

--- server.js
+++ server.js
@@ -11,40 +11,40 @@ app.get("/", (req, res) => {
   res.send("API de Cobro de Cuotas con PostgreSQL 🚀");
 });
 
-// Crear tabla socios si no existe (ejecutar una vez al iniciar)
-// const crearTablas = async () => {
-//   await pool.query(`
-//     CREATE TABLE IF NOT EXISTS socios (
-//       id SERIAL PRIMARY KEY,
-//       nombre TEXT NOT NULL
-//     );
-//   `);
-// };
-// crearTablas();
+//Crear tabla socios si no existe (ejecutar una vez al iniciar)
+const crearTablas = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS socios (
+      id SERIAL PRIMARY KEY,
+      nombre TEXT NOT NULL
+    );
+  `);
+};
+crearTablas();
 
-// // Listar socios
-// app.get("/socios", async (req, res) => {
-//   try {
-//     const result = await pool.query("SELECT * FROM socios ORDER BY id ASC");
-//     res.json(result.rows);
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
+// Listar socios
+app.get("/socios", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM socios ORDER BY id ASC");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
-// // Agregar socio
-// app.post("/socios", async (req, res) => {
-//   try {
-//     const { nombre } = req.body;
-//     const result = await pool.query(
-//       "INSERT INTO socios(nombre) VALUES($1) RETURNING *",
-//       [nombre]
-//     );
-//     res.json(result.rows[0]);
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
+// Agregar socio
+app.post("/socios", async (req, res) => {
+  try {
+    const { nombre } = req.body;
+    const result = await pool.query(
+      "INSERT INTO socios(nombre) VALUES($1) RETURNING *",
+      [nombre]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 // Puerto para Render o local
 const PORT = process.env.PORT || 3000;
