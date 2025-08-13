b1eada13fb73b036697ee50e462ff5d0f96066f5
Dennis Torres <dennisalbertotm@gmail.com>
2025-08-13 19:56:17 -0400

    Retornando a estado anterior

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-// import { Pool } from "pg";
-
-// export const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL, // Render la pone automáticamente
-//   ssl: { rejectUnauthorized: false }          // Necesario para conexión segura
-// });
-
-import pkg from 'pg';
-const { Pool } = pkg;
+import { Pool } from "pg";
 
 export const pool = new Pool({
-  host: 'ep-dawn-frog-acmo2yd4-pooler.sa-east-1.aws.neon.tech',
-  user: 'neondb_owner',
-  password: 'npg_0k7DAxhwLzlP',
-  database: 'neondb',
-  port: 5432,
-  ssl: { rejectUnauthorized: false } // necesario para Neon
+  connectionString: process.env.DATABASE_URL, // Render la pone automáticamente
+  ssl: { rejectUnauthorized: false }          // Necesario para conexión segura
 });
+
+// import pkg from 'pg';
+// const { Pool } = pkg;
+
+// export const pool = new Pool({
+//   host: 'ep-dawn-frog-acmo2yd4-pooler.sa-east-1.aws.neon.tech',
+//   user: 'neondb_owner',
+//   password: 'npg_0k7DAxhwLzlP',
+//   database: 'neondb',
+//   port: 5432,
+//   ssl: { rejectUnauthorized: false } // necesario para Neon
+// });
